e52014ba860b91af2168cca62c2a339f81b5292c
Mordechai Katan <mordechai@sensory.co.il>
2023-03-15 16:49:45 +0200

    second commit

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using static demo_api_old.Models.paymentModels;
+
+namespace demo_api_old.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PaymentController
+    {
+        const string MeshulamPageCode = "539888f537b7";
+        const string MeshulamApiKey = "b60e1d4cbd29";
+        const string MeshulamUserId = "cf2ebf779f618e59";
+        const string MeshulamApiUrl = "https://sandbox.meshulam.co.il/api/light/server/1.0/";
+
+        [HttpPost]
+        public async Task<GenericResultDto> GetPaymentLink([FromBody] GetPaymentLinkRequest req)
+        {
+            var result = new GenericResultDto();
+            var pageCode = "b73ca07591f8";
+            var userId = "4ec1d595ae764243";
+            var successUrl = "https://localhost:44374/Client/success.html?success=1";
+            var failureUrl = "https://localhost:44374/Client/failure.html?failure=1";
+            MultipartFormDataContent form = new MultipartFormDataContent();
+
+            form.Add(new StringContent(pageCode), "pageCode");
+            form.Add(new StringContent(userId), "userId");
+            //form.Add(new StringContent(MeshulamApiKey), "apiKey");
+            form.Add(new StringContent(req.Sum.ToString()), "sum"); // סכום   
+            form.Add(new StringContent(req.PaymentsNum.ToString()), "paymentNum"); // מספר תשלומים
+            form.Add(new StringContent(successUrl), "successUrl");
+            form.Add(new StringContent(failureUrl), "cancelUrl");
+            form.Add(new StringContent(req.Description), "description"); // TODO:: Fix 
+            form.Add(new StringContent(req.Name), "pageField[fullName]");
+            form.Add(new StringContent(req.Phone), "pageField[phone]");
+            form.Add(new StringContent("blabla"), "cField1");
+            form.Add(new StringContent("blabla"), "cField2");
+            form.Add(new StringContent(pageCode), "cField3");
+            form.Add(new StringContent("blabla"), "cField4");
+            form.Add(new StringContent("blabla"), "cField5");
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("multipart/form-data"));
+            var response = await client.PostAsync(MeshulamApiUrl + "createPaymentProcess", form);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var res = JsonConvert.DeserializeObject<GetPaymentLinkResponse>(responseString);
+            result.IsSuccess = res.Status > 0;
+            result.Message = result.IsSuccess ? res.Data.Url : res.Err.message;
+            return result;
+        }
+    }
+}
+
+
---
+++ wwwroot/Client/index.js
@@ -0,0 +1,40 @@
+﻿const apiUrl = 'https://localhost:44374';
+let resultStatus; //boolean
+let resultData;
+
+
+const data = {
+    Sum: 1,
+    PaymentsNum: 1,
+    Description: "The destination of the payment",
+    Name: "John Smit",
+    Phone: "0500000000"
+}
+
+const options = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+}
+
+async function getPaymentLink() {
+    const response = await fetch(`${apiUrl}/api/Payment/GetPaymentLink`, options); //response in format {isSuccess: boolean, message: string (link or error-message)}
+    const data = await response.json();
+    console.log(data.message);
+    window.open(data.message);
+}
+
+window.addEventListener("growWalletChange", (result) => {
+    console.log("result:", result.detail);
+    let res = result.detail;
+    if (res.status === 1) {
+        resultStatus = true;
+        resultData = res.data;
+    }
+    if (res.state === "close" && resultStatus) {
+        console.log(resultData) //or display success page with resultData
+        window.location.href = "/success"
+    }
+});
